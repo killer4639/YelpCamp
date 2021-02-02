ec7035f497c484ea6882b5e22e625c2116b48893
Shiva Gupta <shivagupta@gmail.com>
2021-02-02 21:24:01 +0530

    add start script

--- app.js
+++ app.js
@@ -146,7 +146,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {
         err
     });
-})
-app.listen(3000, () => {
-    console.log(`working`);
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
